0258ed5ca885fb87238f208de0e80ee1ab489105
nanad <32016456+nadhiravirly@users.noreply.github.com>
2024-10-31 23:36:50 +0700

    Create __init__.py
    first creation __init__.py

---
+++ methods/gtan-data1/__init__.py
@@ -0,0 +1,5 @@
+from .gtan_model import GraphAttnModel
+from .gtan_lpa import load_lpa_subtensor
+from .gtan_main import gtan_main, load_gtan_data
+
+__all__ = ['GraphAttnModel', 'load_lpa_subtensor', 'gtan_main', 'load_gtan_data']
