3b8f7798daaf80fa50e6a98465348d2952e5bc7e
nanad <32016456+nadhiravirly@users.noreply.github.com>
2024-10-31 23:33:51 +0700

    Create gtan_main.py
    first creation gtan_main.py

---
+++ methods/gtan-data1/gtan_main.py
@@ -0,0 +1,166 @@
+import numpy as np
+import dgl
+import torch
+import os
+from sklearn.metrics import average_precision_score, f1_score, roc_auc_score
+import torch.optim as optim
+from sklearn.model_selection import StratifiedKFold, train_test_split
+import torch.nn as nn
+from sklearn.preprocessing import LabelEncoder, QuantileTransformer
+from dgl.dataloading import MultiLayerFullNeighborSampler
+from dgl.dataloading import NodeDataLoader
+from .gtan_model import GraphAttnModel
+from . import *
+
+def gtan_main(feat_df, graph, train_idx, test_idx, labels, args, cat_features):
+    device = args['device']
+    graph = graph.to(device)
+    oof_predictions = torch.from_numpy(
+        np.zeros([len(feat_df), 2])).float().to(device)
+    test_predictions = torch.from_numpy(
+        np.zeros([len(feat_df), 2])).float().to(device)
+    kfold = StratifiedKFold(
+        n_splits=args['n_fold'], shuffle=True, random_state=args['seed'])
+
+    y_target = labels.iloc[train_idx].values
+    num_feat = torch.from_numpy(feat_df.values).float().to(device)
+    cat_feat = {col: torch.from_numpy(feat_df[col].values).long().to(
+        device) for col in cat_features}
+
+    y = labels
+    labels = torch.from_numpy(y.values).long().to(device)
+    loss_fn = nn.CrossEntropyLoss().to(device)
+    for fold, (trn_idx, val_idx) in enumerate(kfold.split(feat_df.iloc[train_idx], y_target)):
+        print(f'Training fold {fold + 1}')
+        trn_ind, val_ind = torch.from_numpy(np.array(train_idx)[trn_idx]).long().to(
+            device), torch.from_numpy(np.array(train_idx)[val_idx]).long().to(device)
+
+        train_sampler = MultiLayerFullNeighborSampler(args['n_layers'])
+        train_dataloader = NodeDataLoader(graph,
+                                          trn_ind,
+                                          train_sampler,
+                                          device=device,
+                                          use_ddp=False,
+                                          batch_size=args['batch_size'],
+                                          shuffle=True,
+                                          drop_last=False,
+                                          num_workers=0
+                                          )
+        val_sampler = MultiLayerFullNeighborSampler(args['n_layers'])
+        val_dataloader = NodeDataLoader(graph,
+                                        val_ind,
+                                        val_sampler,
+                                        use_ddp=False,
+                                        device=device,
+                                        batch_size=args['batch_size'],
+                                        shuffle=True,
+                                        drop_last=False,
+                                        num_workers=0,
+                                        )
+        model = GraphAttnModel(in_feats=feat_df.shape[1],
+                               hidden_dim=args['hid_dim']//4,
+                               n_classes=2,
+                               heads=[4]*args['n_layers'],
+                               activation=nn.PReLU(),
+                               n_layers=args['n_layers'],
+                               drop=args['dropout'],
+                               device=device,
+                               gated=args['gated'],
+                               ref_df=feat_df.iloc[train_idx],
+                               cat_features=cat_feat).to(device)
+        lr = args['lr'] * np.sqrt(args['batch_size']/1024)
+        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=args['wd'])
+        lr_scheduler = MultiStepLR(optimizer=optimizer, milestones=[4000, 12000], gamma=0.3)
+
+        earlystoper = early_stopper(patience=args['early_stopping'], verbose=True)
+        for epoch in range(args['max_epochs']):
+            model.train()
+            for step, (input_nodes, seeds, blocks) in enumerate(train_dataloader):
+                batch_inputs, batch_work_inputs, batch_labels, lpa_labels = load_lpa_subtensor(
+                    num_feat, cat_feat, labels, seeds, input_nodes, device)
+                blocks = [block.to(device) for block in blocks]
+                train_batch_logits = model(blocks, batch_inputs, lpa_labels, batch_work_inputs)
+                mask = batch_labels == 2
+                train_batch_logits = train_batch_logits[~mask]
+                batch_labels = batch_labels[~mask]
+
+                train_loss = loss_fn(train_batch_logits, batch_labels)
+                optimizer.zero_grad()
+                train_loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+
+                if step % 10 == 0:
+                    score = torch.softmax(train_batch_logits.clone().detach(), dim=1)[:, 1].cpu().numpy()
+                    print(f'Epoch:{epoch:03d}|Batch:{step:04d}, train_loss:{train_loss:.4f}')
+
+            model.eval()
+            with torch.no_grad():
+                for step, (input_nodes, seeds, blocks) in enumerate(val_dataloader):
+                    batch_inputs, batch_work_inputs, batch_labels, lpa_labels = load_lpa_subtensor(
+                        num_feat, cat_feat, labels, seeds, input_nodes, device)
+                    blocks = [block.to(device) for block in blocks]
+                    val_batch_logits = model(blocks, batch_inputs, lpa_labels, batch_work_inputs)
+                    mask = batch_labels == 2
+                    val_batch_logits = val_batch_logits[~mask]
+                    batch_labels = batch_labels[~mask]
+
+                    val_loss = loss_fn(val_batch_logits, batch_labels)
+                    print(f'Val loss: {val_loss:.4f}')
+
+            earlystoper.earlystop(val_loss, model)
+            if earlystoper.is_earlystop:
+                print("Early Stopping!")
+                break
+
+    print("Best val_loss is: {:.7f}".format(earlystoper.best_cv))
+    
+def load_gtan_data(dataset: str, test_size: float):
+    prefix = os.path.join(os.path.dirname(__file__), "..", "..", "data/")
+    if dataset == "DATA1":
+        cat_features = ["cc_num", "merchant", "category", "city"]
+        df = pd.read_csv(prefix + "DATA1_full.csv")
+        df = df.loc[:, ~df.columns.str.contains('Unnamed')]
+        data = df[df["is_fraud"] <= 2]
+        data = data.reset_index(drop=True)
+
+        # Build the graph structure based on defined pairs
+        out, alls, allt = [], [], []
+        pair = ["cc_num", "merchant", "category", "city"]
+        for column in pair:
+            src, tgt = [], []
+            edge_per_trans = 3
+            for c_id, c_df in data.groupby(column):
+                c_df = c_df.sort_values(by="Time")
+                df_len = len(c_df)
+                sorted_idxs = c_df.index
+                src.extend([sorted_idxs[i] for i in range(df_len)
+                            for j in range(edge_per_trans) if i + j < df_len])
+                tgt.extend([sorted_idxs[i+j] for i in range(df_len)
+                            for j in range(edge_per_trans) if i + j < df_len])
+            alls.extend(src)
+            allt.extend(tgt)
+        alls = np.array(alls)
+        allt = np.array(allt)
+        g = dgl.graph((alls, allt))
+
+        cal_list = ["cc_num", "merchant", "category", "city"]
+        for col in cal_list:
+            le = LabelEncoder()
+            data[col] = le.fit_transform(data[col].apply(str).values)
+        feat_data = data.drop("is_fraud", axis=1)
+        labels = data["is_fraud"]
+
+        feat_data.to_csv(prefix + "DATA1_feat_data.csv", index=None)
+        labels.to_csv(prefix + "DATA1_label_data.csv", index=None)
+
+        index = list(range(len(labels)))
+        g.ndata['label'] = torch.from_numpy(labels.to_numpy()).to(torch.long)
+        g.ndata['feat'] = torch.from_numpy(feat_data.to_numpy()).to(torch.float32)
+        graph_path = prefix + "graph-DATA1.bin"
+        dgl.data.utils.save_graphs(graph_path, [g])
+
+        train_idx, test_idx, y_train, y_test = train_test_split(
+            index, labels, stratify=labels, test_size=test_size/2, random_state=2, shuffle=True)
+    
+    return feat_data, labels, train_idx, test_idx, g, cat_features
