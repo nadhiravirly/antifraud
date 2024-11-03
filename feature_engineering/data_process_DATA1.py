8f6c13d6b65b2ce65b475b2ffd8463ed836fa9aa
Nadhira Virly <32016456+nadhiravirly@users.noreply.github.com>
2024-11-03 18:58:35 +0700

    Update data_process_DATA1.py
    modif sendri

--- feature_engineering/data_process_DATA1.py
+++ feature_engineering/data_process_DATA1.py
@@ -1,20 +1,36 @@
+# %%
 from collections import defaultdict
 import pandas as pd
 import numpy as np
+from scipy.io import loadmat
 import torch
 import dgl
-import os
 import random
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from tqdm import tqdm
+import os
+import time
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+import networkx as nx
+import scipy.sparse as sp
+from sklearn.preprocessing import LabelEncoder
 
-DATADIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data/")
+from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler
+# from . import *
+DATADIR = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", "data/")
+
 
 def featmap_gen(tmp_df=None):
     """
-    Feature engineering on DATA1 dataset.
+    Handle S-FFSD dataset and do some feature engineering
+    :param tmp_df: the feature of input dataset
     """
-    time_span = [2, 3, 5, 15, 20, 50, 100, 150, 200, 300, 864, 2590, 5100, 10000, 24000]
+    # time_span = [2, 5, 12, 20, 60, 120, 300, 600, 1500, 3600, 10800, 32400, 64800, 129600,
+    #              259200]  # Increase in the number of time windows to increase the characteristics.
+    time_span = [2, 3, 5, 15, 20, 50, 100, 150,
+                 200, 300, 864, 2590, 5100, 10000, 24000]
     time_name = [str(i) for i in time_span]
     time_list = tmp_df['Time']
     post_fe = []
@@ -26,22 +42,34 @@ def featmap_gen(tmp_df=None):
             lowbound = (time_list >= temp_time - length)
             upbound = (time_list <= temp_time)
             correct_data = tmp_df[lowbound & upbound]
-            new_df[f'trans_at_avg_{tname}'] = correct_data['amt'].mean()
-            new_df[f'trans_at_totl_{tname}'] = correct_data['amt'].sum()
-            new_df[f'trans_at_std_{tname}'] = correct_data['amt'].std()
-            new_df[f'trans_at_bias_{tname}'] = temp_amt - correct_data['amt'].mean()
-            new_df[f'trans_at_num_{tname}'] = len(correct_data)
-            new_df[f'trans_location_num_{tname}'] = len(correct_data.city.unique())
-            new_df[f'trans_category_num_{tname}'] = len(correct_data.category.unique())
-            new_df[f'trans_merchant_num_{tname}'] = len(correct_data.merchant.unique())
+            new_df['trans_at_avg_{}'.format(
+                tname)] = correct_data['amt'].mean()
+            new_df['trans_at_totl_{}'.format(
+                tname)] = correct_data['amt'].sum()
+            new_df['trans_at_std_{}'.format(
+                tname)] = correct_data['amt'].std()
+            new_df['trans_at_bias_{}'.format(
+                tname)] = temp_amt - correct_data['amt'].mean()
+            new_df['trans_at_num_{}'.format(tname)] = len(correct_data)
+            new_df['trans_merchant_num_{}'.format(tname)] = len(
+                correct_data.merchant.unique())
+            new_df['trans_city_num_{}'.format(tname)] = len(
+                correct_data.city.unique())
+            new_df['trans_category_num_{}'.format(tname)] = len(
+                correct_data.category.unique())
         post_fe.append(new_df)
     return pd.DataFrame(post_fe)
 
+
 def sparse_to_adjlist(sp_matrix, filename):
     """
-    Convert sparse matrix to adjacency list and save to file.
+    Transfer sparse matrix to adjacency list
+    :param sp_matrix: the sparse matrix
+    :param filename: the filename of adjlist
     """
+    # add self loop
     homo_adj = sp_matrix + sp.eye(sp_matrix.shape[0])
+    # create adj_list
     adj_lists = defaultdict(set)
     edges = homo_adj.nonzero()
     for index, node in enumerate(edges[0]):
@@ -49,6 +77,8 @@ def sparse_to_adjlist(sp_matrix, filename):
         adj_lists[edges[1][index]].add(node)
     with open(filename, 'wb') as file:
         pickle.dump(adj_lists, file)
+    file.close()
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -59,94 +89,264 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
+
 def MinMaxScaling(data):
     mind, maxd = data.min(), data.max()
+    # return mind + (data - mind) / (maxd - mind)
     return (data - mind) / (maxd - mind)
 
-def k_neighs(graph: dgl.DGLGraph, center_idx: int, k: int, where: str, choose_risk: bool = False, risk_label: int = 1) -> torch.Tensor:
-    """Return indices of k-hop neighbors with optional risk filtering."""
+
+def k_neighs(
+    graph: dgl.DGLGraph,
+    center_idx: int,
+    k: int,
+    where: str,
+    choose_risk: bool = False,
+    risk_label: int = 1
+) -> torch.Tensor:
+    """return indices of risk k-hop neighbors
+
+    Args:
+        graph (dgl.DGLGraph): dgl graph dataset
+        center_idx (int): center node idx
+        k (int): k-hop neighs
+        where (str): {"predecessor", "successor"}
+        risk_label (int, optional): value of fruad label. Defaults to 1.
+    """
+    merchant_idxs: torch.Tensor
     if k == 1:
-        neigh_idxs = graph.predecessors(center_idx) if where == "in" else graph.successors(center_idx)
+        if where == "in":
+            neigh_idxs = graph.predecessors(center_idx)
+        elif where == "out":
+            neigh_idxs = graph.successors(center_idx)
+
     elif k == 2:
-        subg = dgl.khop_in_subgraph(graph, center_idx, 2, store_ids=True)[0] if where == "in" else dgl.khop_out_subgraph(graph, center_idx, 2, store_ids=True)[0]
-        neigh_idxs = subg.ndata[dgl.NID][subg.ndata[dgl.NID] != center_idx]
-        neigh1s = graph.predecessors(center_idx) if where == "in" else graph.successors(center_idx)
-        neigh_idxs = neigh_idxs[~torch.isin(neigh_idxs, neigh1s)]
+        if where == "in":
+            subg_in = dgl.khop_in_subgraph(
+                graph, center_idx, 2, store_ids=True)[0]
+            neigh_idxs = subg_in.ndata[dgl.NID][subg_in.ndata[dgl.NID] != center_idx]
+            # delete center node itself
+            neigh1s = graph.predecessors(center_idx)
+            neigh_idxs = neigh_idxs[~torch.isin(neigh_idxs, neigh1s)]
+        elif where == "out":
+            subg_out = dgl.khop_out_subgraph(
+                graph, center_idx, 2, store_ids=True)[0]
+            neigh_idxs = subg_out.ndata[dgl.NID][subg_in.ndata[dgl.NID] != center_idx]
+            neigh1s = graph.successors(center_idx)
+            neigh_idxs = neigh_idxs[~torch.isin(neigh_idxs, neigh1s)]
 
-    return neigh_idxs[graph.ndata['label'][neigh_idxs] == risk_label] if choose_risk else neigh_idxs
+    neigh_labels = graph.ndata['label'][neigh_idxs]
+    if choose_risk:
+        merchant_idxs = neigh_idxs[neigh_labels == risk_label]
+    else:
+        merchant_idxs = neigh_idxs
 
-def count_risk_neighs(graph: dgl.DGLGraph, risk_label: int = 1) -> torch.Tensor:
-    """
-    Count the number of risk neighbors for each node.
-    """
+    return merchant_idxs
+
+
+def count_risk_neighs(
+    graph: dgl.DGLGraph,
+    risk_label: int = 1
+) -> torch.Tensor:
+
     ret = []
     for center_idx in graph.nodes():
         neigh_idxs = graph.successors(center_idx)
-        ret.append((graph.ndata['label'][neigh_idxs] == risk_label).sum())
+        neigh_labels = graph.ndata['label'][neigh_idxs]
+        risk_neigh_num = (neigh_labels == risk_label).sum()
+        ret.append(risk_neigh_num)
+
     return torch.Tensor(ret)
 
-def feat_map(graph: dgl.DGLGraph, edge_feat: torch.Tensor):
-    """Generate neighbor-based features for nodes."""
+
+def feat_map():
     tensor_list = []
-    feat_names = ["1hop_degree", "2hop_degree", "1hop_riskstat", "2hop_riskstat"]
+    feat_names = []
     for idx in tqdm(range(graph.num_nodes())):
         neighs_1_of_center = k_neighs(graph, idx, 1, "in")
         neighs_2_of_center = k_neighs(graph, idx, 2, "in")
+
         tensor = torch.FloatTensor([
             edge_feat[neighs_1_of_center, 0].sum().item(),
+            # edge_feat[neighs_1_of_center, 0].std().item(),
             edge_feat[neighs_2_of_center, 0].sum().item(),
+            # edge_feat[neighs_2_of_center, 0].std().item(),
             edge_feat[neighs_1_of_center, 1].sum().item(),
+            # edge_feat[neighs_1_of_center, 1].std().item(),
             edge_feat[neighs_2_of_center, 1].sum().item(),
+            # edge_feat[neighs_2_of_center, 1].std().item(),
         ])
         tensor_list.append(tensor)
-    return torch.stack(tensor_list), feat_names
 
-def process_graph_data(data, output_graph_file):
-    """Process DATA1 to create the graph and features."""
+    feat_names = ["1hop_degree", "2hop_degree",
+                  "1hop_riskstat", "2hop_riskstat"]
+
+    tensor_list = torch.stack(tensor_list)
+    return tensor_list, feat_names
+
+
+if __name__ == "__main__":
+
+    set_seed(42)
+
+    # %%
+    #"""
+    #    For Yelpchi dataset
+    #    Code partially from https://github.com/YingtongDou/CARE-GNN
+    #"""
+    #print(f"processing YELP data...")
+    #yelp = loadmat(os.path.join(DATADIR, 'YelpChi.mat'))
+    #net_rur = yelp['net_rur']
+    #net_rtr = yelp['net_rtr']
+    #net_rsr = yelp['net_rsr']
+    #yelp_homo = yelp['homo']
+
+    #sparse_to_adjlist(net_rur, os.path.join(
+    #    DATADIR, "yelp_rur_adjlists.pickle"))
+    #sparse_to_adjlist(net_rtr, os.path.join(
+    #    DATADIR, "yelp_rtr_adjlists.pickle"))
+    #sparse_to_adjlist(net_rtr, os.path.join(
+    #    DATADIR, "yelp_rsr_adjlists.pickle"))
+    #sparse_to_adjlist(net_rtr, os.path.join(
+    #    DATADIR, "yelp_homo_adjlists.pickle"))
+
+    #data_file = yelp
+    #labels = pd.DataFrame(data_file['label'].flatten())[0]
+    #feat_data = pd.DataFrame(data_file['features'].todense().A)
+    ## load the preprocessed adj_lists
+    #with open(os.path.join(DATADIR, "yelp_homo_adjlists.pickle"), 'rb') as file:
+    #    homo = pickle.load(file)
+    #file.close()
+    #src = []
+    #tgt = []
+    #for i in homo:
+    #    for j in homo[i]:
+    #        src.append(i)
+    #        tgt.append(j)
+    #src = np.array(src)
+    #tgt = np.array(tgt)
+    #g = dgl.graph((src, tgt))
+    #g.ndata['label'] = torch.from_numpy(labels.to_numpy()).to(torch.long)
+    #g.ndata['feat'] = torch.from_numpy(
+    #    feat_data.to_numpy()).to(torch.float32)
+    #dgl.data.utils.save_graphs(DATADIR + "graph-yelp.bin", [g])
+
+    # %%
+    #"""
+    #    For Amazon dataset
+    #"""
+    #print(f"processing AMAZON data...")
+    #amz = loadmat(os.path.join(DATADIR, 'Amazon.mat'))
+    #net_upu = amz['net_upu']
+    #net_usu = amz['net_usu']
+    #net_uvu = amz['net_uvu']
+    #amz_homo = amz['homo']
+    #
+    #sparse_to_adjlist(net_upu, os.path.join(
+    #    DATADIR, "amz_upu_adjlists.pickle"))
+    #sparse_to_adjlist(net_usu, os.path.join(
+    #    DATADIR, "amz_usu_adjlists.pickle"))
+    #sparse_to_adjlist(net_uvu, os.path.join(
+    #    DATADIR, "amz_uvu_adjlists.pickle"))
+    #sparse_to_adjlist(amz_homo, os.path.join(
+    #    DATADIR, "amz_homo_adjlists.pickle"))
+    #
+    #data_file = amz
+    #labels = pd.DataFrame(data_file['label'].flatten())[0]
+    #feat_data = pd.DataFrame(data_file['features'].todense().A)
+    ## load the preprocessed adj_lists
+    #with open(DATADIR + 'amz_homo_adjlists.pickle', 'rb') as file:
+    #    homo = pickle.load(file)
+    #file.close()
+    #src = []
+    #tgt = []
+    #for i in homo:
+    #    for j in homo[i]:
+    #        src.append(i)
+    #        tgt.append(j)
+    #src = np.array(src)
+    #tgt = np.array(tgt)
+    #g = dgl.graph((src, tgt))
+    #g.ndata['label'] = torch.from_numpy(labels.to_numpy()).to(torch.long)
+    #g.ndata['feat'] = torch.from_numpy(
+    #    feat_data.to_numpy()).to(torch.float32)
+    #dgl.data.utils.save_graphs(DATADIR + "graph-amazon.bin", [g])
+
+    # # %%
+    # """
+    #     For DATA1 dataset
+    # """
+    print(f"processing S-FFSD data...")
+    data = pd.read_csv(os.path.join(DATADIR, 'DATA1.csv'))
     data = featmap_gen(data.reset_index(drop=True))
     data.replace(np.nan, 0, inplace=True)
     data.to_csv(os.path.join(DATADIR, 'DATA1_full.csv'), index=None)
+    data = pd.read_csv(os.path.join(DATADIR, 'DATA1_full.csv'))
 
     data = data.reset_index(drop=True)
-    alls, allt = [], []
-    pair = ["cc_num", "merchant", "category", "city"]
+    out = []
+    alls = []
+    allt = []
+    pair = ["cc_numn", "merchant", "city", "category"]
     for column in pair:
         src, tgt = [], []
         edge_per_trans = 3
         for c_id, c_df in tqdm(data.groupby(column), desc=column):
             c_df = c_df.sort_values(by="Time")
+            df_len = len(c_df)
             sorted_idxs = c_df.index
-            src.extend([sorted_idxs[i] for i in range(len(c_df)) for j in range(edge_per_trans) if i + j < len(c_df)])
-            tgt.extend([sorted_idxs[i + j] for i in range(len(c_df)) for j in range(edge_per_trans) if i + j < len(c_df)])
+            src.extend([sorted_idxs[i] for i in range(df_len)
+                        for j in range(edge_per_trans) if i + j < df_len])
+            tgt.extend([sorted_idxs[i+j] for i in range(df_len)
+                        for j in range(edge_per_trans) if i + j < df_len])
         alls.extend(src)
         allt.extend(tgt)
-
-    g = dgl.graph((np.array(alls), np.array(allt)))
-    for col in ["cc_num", "merchant", "category", "city"]:
+    alls = np.array(alls)
+    allt = np.array(allt)
+    g = dgl.graph((alls, allt))
+    cal_list = ["cc_numn", "merchant", "city", "category"]
+    for col in cal_list:
         le = LabelEncoder()
         data[col] = le.fit_transform(data[col].apply(str).values)
+    feat_data = data.drop("Labels", axis=1)
+    labels = data["Labels"]
+    g.ndata['label'] = torch.from_numpy(
+        labels.to_numpy()).to(torch.long)
+    g.ndata['feat'] = torch.from_numpy(
+        feat_data.to_numpy()).to(torch.float32)
+    dgl.data.utils.save_graphs(DATADIR + "graph-DATA1.bin", [g])
 
-    feat_data = data.drop("is_fraud", axis=1).apply(pd.to_numeric, errors='coerce').fillna(0)
-    labels = data["is_fraud"]
-    g.ndata['label'] = torch.from_numpy(labels.to_numpy()).to(torch.long)
-    g.ndata['feat'] = torch.from_numpy(feat_data.to_numpy()).to(torch.float32)
-    dgl.data.utils.save_graphs(DATADIR + output_graph_file, [g])
+    # generate neighbor riskstat features
+    #for file_name in ['S-FFSD', 'yelp', 'amazon']:
+    for file_name in ['DATA1']:
+        print(
+            f"Generating neighbor risk-aware features for {file_name} dataset...")
+        graph = dgl.load_graphs(DATADIR + "graph-" + file_name + ".bin")[0][0]
+        graph: dgl.DGLGraph
+        print(f"graph info: {graph}")
 
-    generate_neighbor_features(g)
+        edge_feat: torch.Tensor
+        degree_feat = graph.in_degrees().unsqueeze_(1).float()
+        risk_feat = count_risk_neighs(graph).unsqueeze_(1).float()
 
-def generate_neighbor_features(graph):
-    """Generate neighbor-based features for each node."""
-    degree_feat = graph.in_degrees().unsqueeze_(1).float()
-    risk_feat = count_risk_neighs(graph).unsqueeze_(1).float()
+        origin_feat_name = []
+        edge_feat = torch.cat([degree_feat, risk_feat], dim=1)
+        origin_feat_name = ['degree', 'riskstat']
 
-    edge_feat = torch.cat([degree_feat, risk_feat], dim=1)
-    features_neigh, feat_names = feat_map(graph, edge_feat)
-    features_neigh = torch.cat((edge_feat, features_neigh), dim=1).numpy()
-    features_neigh = pd.DataFrame(features_neigh, columns=['degree', 'riskstat'] + feat_names)
-    scaler = StandardScaler()
-    features_neigh = pd.DataFrame(scaler.fit_transform(features_neigh), columns=features_neigh.columns)
-    features_neigh.to_csv(os.path.join(DATADIR, "DATA1_neigh_feat.csv"), index=False)
+        features_neigh, feat_names = feat_map()
+        # print(f"feature neigh: {features_neigh.shape}")
 
-if __name__ == "__main__":
-    data = pd.read_csv(os.path.join(DATADIR, 'DATA1.csv'))
-    process_graph_data(data, "graph-DATA1.bin")
+        features_neigh = torch.cat(
+            (edge_feat, features_neigh), dim=1
+        ).numpy()
+        feat_names = origin_feat_name + feat_names
+        features_neigh[np.isnan(features_neigh)] = 0.
+
+        output_path = DATADIR + file_name + "_neigh_feat.csv"
+        features_neigh = pd.DataFrame(features_neigh, columns=feat_names)
+        scaler = StandardScaler()
+        # features_neigh = np.log(features_neigh + 1)
+        features_neigh = pd.DataFrame(scaler.fit_transform(
+            features_neigh), columns=features_neigh.columns)
+
+        features_neigh.to_csv(output_path, index=False)
